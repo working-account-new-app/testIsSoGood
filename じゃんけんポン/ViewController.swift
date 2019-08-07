f0fceb6107bd926424721a67df4e580061b2460d
キムタ <kimuyou01@gmail.com>
2019-08-07 13:15:49 +0400

    Initial Commit

---
+++ じゃんけんポン/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  じゃんけんポン
+//
+//  Created by 木村　優希 on 2019/08/07.
+//  Copyright © 2019 木村　優希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var rock: UIButton!
+    
+   
+    @IBOutlet weak var sissoes: UIButton!
+    
+    @IBOutlet weak var paper: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
